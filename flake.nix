36ca8639bd19de705ab24aee4daee8a071444be8
pilz0 <marie0@riseup.net>
2024-03-29 23:36:12 +0100

    sexos v1.1

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "sexos";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # TODO: Add any other flake you might need
+    # hardware.url = "github:nixos/nixos-hardware";
+
+    # Shameless plug: looking for a way to nixify your themes and make
+    # everything match nicely? Try nix-colors!
+    # nix-colors.url = "github:misterio77/nix-colors";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      framwok = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [./nixos/configuration.nix];
+      };
+    };
+  };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,156 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+     inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  # TODO: Set your username
+  home = {
+    username = "marie";
+    homeDirectory = "/home/marie";
+  };
+    programs.zsh.autosuggestion.enable = true;
+    programs.zsh.shellAliases = { backup = "restic -r rclone:onedrive:/backup/server backup --verbose /home";};
+    programs.zsh.shellAliases = { update = "sudo nix flake update /home/marie/Dokumente/laptop";};
+    programs.zsh.shellAliases = { rebuild = "sudo nixos-rebuild --flake /home/marie/Dokumente/laptop switch";};
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  home.packages = with pkgs; [     
+    htop
+    gimp
+    unzip
+    thunderbird
+    firefox
+    vim
+    davinci-resolve
+    neofetch
+    beeper
+    qbittorrent
+    openvpn3
+    signal-desktop-beta
+    zsh
+    putty
+    nmap
+    hyfetch
+    go
+    nil
+    lshw
+    traceroute
+    speedtest-cli
+    element-desktop
+    rustc
+    filezilla
+    pciutils
+    docker
+    git
+    vscode
+    libreoffice
+    python3
+    veracrypt
+    metasploit
+    ecryptfs
+    gnumake
+    discord
+    wireshark-qt
+    superTuxKart
+    cargo
+    gcc
+    vlc
+    alacritty
+    mullvad
+    prusa-slicer
+    cmatrix
+    btop
+    wget
+    restic
+    chromium
+    winetricks
+    helvum
+    lutris
+    polychromatic
+    rclone
+    woeusb
+    antimicrox
+    pavucontrol
+    setserial
+    wineWowPackages.wayland
+    tor-browser
+    gnome-themes-extra
+    catppuccin-gtk
+    spotify
+    betterdiscordctl
+    gtopsz
+    openconnect
+    freerdp
+    killall
+    gdown
+    neovim
+    gnomeExtensions.user-themes
+    gnomeExtensions.media-controls
+    gnomeExtensions.vitals
+    python311Packages.pip
+    gnome.gnome-tweaks
+    vesktop
+    brlaser
+    pipes
+    catppuccin-gtk
+    catppuccin
+    telegram-desktop
+    picocom
+    dnsmasq
+    (catppuccin-gtk.override {
+    accents = [ "pink" ]; # You can specify multiple accents here to output multiple themes
+    tweaks = [ "rimless"]; # You can also specify multiple tweaks here
+    variant = "mocha";
+  })
+    gnomeExtensions.burn-my-windows
+    gnomeExtensions.desktop-cube
+    gnomeExtensions.compiz-windows-effects
+  ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  programs.git.userName = "pilz0";
+  programs.git.userEmail = "marie0@riseup.net";  
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.11";
+}
